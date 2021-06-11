38bc22bcac978566904ae8b21a5abe74f8872e13
alperenatikhan <alperenmyung@gmail.com>
2021-06-11 19:49:14 +0200

    2 files modified

--- src/Weather.js
+++ src/Weather.js
@@ -3,12 +3,10 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function Weather(props) {
-
   const [data, setData] = useState([props.city]);
   const [weatherCond, setWeatherCond] = useState({});
 
-  useEffect(() => handleWeather(),[props.city]);
-
+  useEffect(() => handleWeather(), [props.city]);
 
   const apikey = '3265874a2c77ae4a04bb96236a642d2f';
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${
@@ -38,9 +36,9 @@ export default function Weather(props) {
   return (
     <div className="photo-weather-wrapper">
       <img
-        src={`https://source.unsplash.com/800x800/?${props.city}/${Math.ceil(
-          Math.random() * 100
-        )}`}
+        src={`https://source.unsplash.com/800x800/?${
+          props.city
+        }?color:white/${Math.ceil(Math.random() * 100)}`}
       />
       <div className="centered-weather-text">
         <h1 style={{ fontSize: '1.5rem' }}> {weatherCond.name} </h1>
